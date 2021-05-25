b4e985cb0539913fc4119bc8a9fa75667b5f67c4
Gabriela Wojcik <gabriela.wojcik@gmail.com>
2021-05-25 23:11:48 +0200

    added functions

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
 {
     const tasks = [
         {
-            content:
+            content: "nagrać ekcje",
+            done:false
         },
-        {},
     ];
+
+
     const render = () => {
         let htmlString = "";
 
-        for(const task of tasks) {
+        for (const task of tasks) {
             htmlString += `
             <li>
                 ${task.content}
             </li>
             `;
         }
-        document.querySelector("js-tasks").innerHTML = htmlString;
+        document.querySelector(".js-tasks").innerHTML = htmlString;
     };
+
     
     const init = () => {
-        reneder();
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) =>{
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+
+            if (newTaskContent === "") {
+                return;
+            }
+
+            tasks.push({
+                content: newTaskContent,
+            })
+            render();
+        });
     };
 
     init();
