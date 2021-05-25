23c693bce3b29e2db11b65713f9784151a933405
Gabriela Wojcik <gabriela.wojcik@gmail.com>
2021-05-26 00:12:28 +0200

    remove tsk

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 {
     const tasks = [];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        })
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+                render();
+    };
 
     const render = () => {
         let htmlString = "";
@@ -8,19 +19,24 @@
         for (const task of tasks) {
             htmlString += `
             <li>
-                ${task.content}
+            <button class="js-remove">usuń</button>   
+            ${task.content}
             </li>
             `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove")
+    
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+        
     };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        })
-        render();
-    };
+    
 
     const onFormSubmit = (event) =>{
         event.preventDefault();
